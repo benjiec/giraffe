800576f74c598a856d5628ff0db649305ca84196
Benjie Chen <benjie@ginkgobioworks.com>
2013-07-01 21:51:46 -0400

    handle whitespace in front of LOCUS

--- src/giraffe/views.py
+++ src/giraffe/views.py
@@ -22,7 +22,7 @@ def _post(request):
     # parse genbank
     if sequence.strip().startswith('LOCUS'):
       is_gb = True
-      sequence, gb_features = gb.parse_genbank(sequence)
+      sequence, gb_features = gb.parse_genbank(sequence.lstrip())
 
     # clean sequence
     sequence = features.clean_dna_sequence(sequence)
